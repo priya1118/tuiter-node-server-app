dc96387f446abaa6a935a50e139e71a5d2427c9c
Priyanka Padinam <padinampriya@gmail.com>
2023-04-08 12:36:44 -0400

    added dislikes

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,12 +1,11 @@
-import * as tuitsDao from '../../tuits/tuits-dao.js'
+import * as tuitsDao from './tuits-dao.js'
 
 const createTuit = async (req, res) => {
      const newTuit = req.body;
      newTuit.likes = 0;
      newTuit.dislikes = 0;
      newTuit.liked = false;
-     const insertedTuit = await tuitsDao
-                             .createTuit(newTuit);
+     const insertedTuit = await tuitsDao.createTuit(newTuit);
      res.json(insertedTuit);
 }
 const findTuits  = async (req, res) => {
@@ -16,16 +15,14 @@ const findTuits  = async (req, res) => {
 const updateTuit = async (req, res) => {
   const tuitdIdToUpdate = req.params.tid;
   const updates = req.body;
-  const status = await tuitsDao
-                         .updateTuit(tuitdIdToUpdate,
+  const status = await tuitsDao.updateTuit(tuitdIdToUpdate,
                                      updates);
   res.json(status);
 
 }
 const deleteTuit = async (req, res) => {
       const tuitdIdToDelete = req.params.tid;
-      const status = await tuitsDao
-                             .deleteTuit(tuitdIdToDelete);
+      const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
       res.json(status);
 
 }
--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -3,5 +3,6 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  dislikes: Number,
 }, {collection: 'tuits'});
 export default schema;
